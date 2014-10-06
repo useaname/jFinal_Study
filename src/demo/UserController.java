6856932b76ab40aa1a92714376fbe78fcc718680
Hamlin Cheng <cxq4github@163.com>
2014-10-06 22:07:39 +0800

    add

--- src/demo/UserController.java
+++ src/demo/UserController.java
@@ -1,11 +1,18 @@
 package demo;
 
 import com.jfinal.core.Controller;
+import com.jfinal.kit.StrKit;
 import com.jfinal.plugin.activerecord.Page;
 
 import demo.model.User;
 
 public class UserController extends Controller {
+	
+	public void index(){
+		list();
+	}
+	
+	
 	public void list(){
 		int pageNumber = this.getParaToInt(0, 1);
 		int pageSize = this.getParaToInt(1,10);
@@ -20,7 +27,7 @@ public class UserController extends Controller {
 		int id = user.addUser();
 		//int id = User.dao.addUser(name);
 		if(id>0){
-			renderJson("{\"status\":1, \"id\":"+id+"}");
+			renderJson("{ \"status\":1, \"id\":"+id+"}");
 		}else{
 			renderJson("{\"status\":0}");
 		}
@@ -36,4 +43,38 @@ public class UserController extends Controller {
 			renderJson("{\"status\":0}");
 		}
 	}
+	
+	public void del(){
+		User user = getModel(User.class);
+		boolean res = user.delete();
+		if(res){
+			renderJson("{\"status\":1}");
+		}else{
+			renderJson("{\"status\":0}");
+		}
+	}
+	
+	/*手动传入id*/
+	public void getOneUser(){
+		int uid = this.getParaToInt("id");
+		User user  = new User();
+		user = user.findUserById(uid);
+		if(StrKit.notNull(user)){
+			renderJson(user);
+		}else{
+			renderJson("{}");
+		}
+	}
+	
+	/*getModel传入Id*/
+	public void getOneUser2(){
+		User user = getModel(User.class);
+		user = user.findById();
+		if(StrKit.notNull(user)){
+			renderJson(user);
+		}else{
+			renderJson("{}");
+		}
+	}
+	
 }
--- src/demo/model/User.java
+++ src/demo/model/User.java
@@ -7,20 +7,49 @@ public class User extends Model<User> {
 	
 	public static final User dao = new User();
 	
+	/*分页用户查询*/
 	public Page<User> getUser(int pageNumber,int pageSize){
 		Page<User> page = dao.paginate(pageNumber, pageSize, "select *", "from user");
 		return page;
 	}
 	
-	
+	/*增加用户*/
 	public int addUser(){
 		this.save();
 		return this.get("id");
 	}
 	
+	/*更新用户信息*/
 	public int updateUser(){
 		this.update();
 		return this.get("id");
+	}
+	
+	/*删除用户*/
+	public boolean  delete(){
+		boolean res = dao.deleteById(this.get("id"));
+		return res;
+	}
+	
+	/*查询用户1*/
+	public User findById(){
+		String sql = "select * from user where id=?";
+		User user = dao.findFirst(sql, this.get("id"));
+		return user;
+	}
+	
+	/*查询用户2*/
+	public User findUserById(){
+		User user = dao.findById(this.get("id"));
+		return user;
+	}
+	
+	/*查询用户3*/
+	public User findUserById(int id){
+		String sql = "select * from user where id="+id;
+		User user = dao.findFirst(sql);
+		System.out.println(user);
+		return user;
 	}
 }
 
